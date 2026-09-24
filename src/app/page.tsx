1ae48e67224f1f7543c72acd48c3ebe70018cada
Rauf <therauf.dev@gmail.com>
2026-09-24 23:02:50 +0600

    Nav bar design compleated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,74 @@
+import Logo from "@/assets/logo.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <>
+      <nav className="sticky top-0 z-50 w-full border-b border-[#1c1d20]">
+        <div className="container mx-auto grid h-[74px] grid-cols-3 items-center px-4 sm:px-6 lg:px-8">
+
+
+          <div className="flex items-center gap-2">
+            <Link href="/">
+              <Image src={Logo} alt="Logo" />
+            </Link>
+
+            <span className="text-[18px] font-bold text-white">
+              FITLOG
+            </span>
+          </div>
+
+
+          <div className="flex justify-center">
+            <Link
+              href="/"
+              className="flex h-[35px] items-center rounded-full bg-[#182400] px-4 text-[14px] leading-6 text-primary"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              Workouts
+            </Link>
+
+            <Link
+              href="/my-plan"
+              className="flex h-[35px] items-center px-4 text-[13px] leading-6 text-text"
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              My Plan
+            </Link>
+          </div>
+
+
+          <div className="flex justify-end gap-5">
+
+            <div className="flex items-center gap-2">
+              <span className="text-[13px] text-secondary">
+                Plan
+              </span>
+
+              <span className="flex h-[20px] w-[20px] items-center justify-center rounded-full bg-primary text-[11px] leading-0 text-black">
+                0
+              </span>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <span className="text-[13px] text-[#b0b1b5]">
+                Saved
+              </span>
+
+              <span className="flex h-[20px] w-[20px] items-center justify-center rounded-full border border-[#303136] text-[11px] leading-0 text-secondary">
+                0
+              </span>
+            </div>
+
+          </div>
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </nav>
+
+      <section className="container mx-auto px-4 py-5 sm:px-6 lg:px-8">
+        <div className="flex min-h-[380px]">
+
         </div>
-      </main>
-    </div>
+      </section>
+    </>
   );
 }
